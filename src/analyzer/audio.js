350a39bec311028541271dda4a069bc6efbcc70d
samssi <samir.hamadi@gmail.com>
2017-12-20 18:51:36 +0200

    Initial switch position chooser

--- src/analyzer/audio.js
+++ src/analyzer/audio.js
@@ -5,12 +5,13 @@ let prevTime = Date.now();
 let bufferArray = [];
 let arraySize = 0;
 const arrayMax = 100;
-/*const ignoreHighRange = [0];
-const highRange = [1,2,3];
-const midRange = [4,5,6];
-const lowRange = [7,8,9];
-const boomRange = [10,11,12];
-const ignoreLow = [13,14,15];*/
+
+const settings = {
+    highThresholdMin: 180,
+    midThresholdMin: 140,
+    lowThresholdMin: 100,
+    boomThresholdMin: 10,
+}
 
 export const render = () => {
     const audioContext = new AudioContext();
@@ -28,40 +29,39 @@ export const render = () => {
     const renderFrame = () => {
        requestAnimationFrame(renderFrame);
        analyser.getByteFrequencyData(frequencyData);
-       const highRange = calculateAverageFromFrequencyData([frequencyData[1,2,3]]);
-       const midRange = calculateAverageFromFrequencyData([frequencyData[4,5,6]]);
-       const lowRange = calculateAverageFromFrequencyData([frequencyData[7,8,9]]);
-       const boomRange = calculateAverageFromFrequencyData([frequencyData[10,11,12]]);
-       console.log([highRange, midRange, lowRange, boomRange]);
+       const highRange = calculateAverageFromFrequencyData([frequencyData[1], frequencyData[2], frequencyData[3]]);
+       const midRange = calculateAverageFromFrequencyData([frequencyData[4], frequencyData[5], frequencyData[6]]);
+       const lowRange = calculateAverageFromFrequencyData([frequencyData[7], frequencyData[8], frequencyData[9]]);
+       const boomRange = calculateAverageFromFrequencyData([frequencyData[13]]);
+       //console.log(frequencyData[1], frequencyData[2], frequencyData[3]);
+       console.log(relayPositions(highRange, midRange, lowRange, boomRange));
+       //relayPositions(highRange, midRange, lowRange, boomRange)
     }
     audio.play();
     renderFrame();
 }
 
+const relayPositions = (high, mid, low, boom) => {
+    return {
+        high: visualize(high, settings.highThresholdMin),
+        mid: visualize(mid, settings.midThresholdMin),
+        low: visualize(low, settings.lowThresholdMin),
+        boom: visualize(boom, settings.boomThresholdMin)
+    }
+}
+
+const visualize = (frequency, minThreshold) => frequency > minThreshold ? "on" : "off";
+
 const buffer = (chunk) => {
     bufferArray = bufferArray.concat(chunk);
 }
 
-const returnRelayType = (averageFrequency) => {
-    if (averageFrequency > 150) {
-        return "low";
-    }
-    else if (averageFrequency > 108 && averageFrequency < 150) {
-        return "mid";
-    }
-    else if (averageFrequency > 0 && averageFrequency < 500) {
-        return "high";
-    }
-    else {
-        console.log(averageFrequency);
-        return undefined;
-    }
-}
 
-const calculateAverageFromFrequencyData = (frequencyData) => {
+
+const calculateAverageFromFrequencyData = (frequencies) => {
     let sum = 0;
-    for (let i = 0; i < frequencyData.length; i++) {
-        sum = sum + frequencyData[i];
+    for (let i = 0; i < frequencies.length; i++) {
+        sum = sum + frequencies[i];
     }
-    return Math.round(sum / frequencyData.length);
+    return Math.round(sum / frequencies.length);
 }
