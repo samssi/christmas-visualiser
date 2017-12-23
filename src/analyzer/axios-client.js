df5530b406fd0e226fe47a8113e1b6b0547e71de
Sämir Hamadi <samir.hamadi@gofore.com>
2017-12-24 01:50:46 +0200

    sweet spot in settings

--- src/analyzer/axios-client.js
+++ src/analyzer/axios-client.js
@@ -5,5 +5,5 @@ export const relayClient = axios.create({
     //baseURL: "http://192.168.100.14:5000",
     baseURL: "http://192.168.43.233:5000",
     // TODO: timeout should equal sample time
-    timeout: 80
+    timeout: 110
 });
